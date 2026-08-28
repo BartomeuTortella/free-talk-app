642532e4316b484af503ffdaf225a866afdc7790
Bartomeu <bartomeutg@gmail.com>
2026-08-28 19:15:04 +0200

    Added mongoDB connection.

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,29 @@
+import * as dotenv from 'dotenv';
+dotenv.config();
+
 import express from "express";
+import mongoose from 'mongoose';
+
 
 const app = express();
 
-app.listen(8080, () => console.log('Server is up and running on port 8080'));
+app.use(express.urlencoded());
+app.use(express.json());
+
+const start = () => {
+    if (!process.env.MONGO_URI) {
+        throw new Error("MONGO_URI is required!");
+    }
+
+    try {
+        mongoose.connect(process.env.MONGO_URI)
+    } catch (err) {
+        throw new Error('Database error');
+    }
+
+    app.listen(8080, () => console.log('Server is up and running on port 8080'));
+}
+
+
+start();
+
